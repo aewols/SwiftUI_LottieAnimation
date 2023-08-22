efdb8ea45456b32589925276d6b2a1cb329c7f90
aewols <i0y0j057@gmail.com>
2023-08-23 02:45:45 +0900

    Initial Commit

---
+++ LottieAnimation/LottieAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieAnimationApp.swift
+//  LottieAnimation
+//
+//  Created by 이연정 on 2023/08/23.
+//
+
+import SwiftUI
+
+@main
+struct LottieAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
